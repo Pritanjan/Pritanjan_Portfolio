d2b3b728771bf87e33efaa44db097deb9b8326f6
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2024-08-28 14:14:02 +0530

    furth

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -1,13 +1,41 @@
-import React from 'react';
+// import React from "react";
 
-function ProjectCard({ src, link, h3, h4, p }) {
+// function ProjectCard({ src, link, h3, h4, p }) {
+//   return (
+//     <a href={link} target="_blank">
+//       {/* <a href={githubLink} target="_blank"></a> */}
+//       <img className="hover" src={src} alt={`${h3} logo`} />
+//       <h3>{h3}</h3>
+//       <h4>{h4}</h4>
+//       <p>{p}</p>
+//     </a>
+//   );
+// }
+
+// export default ProjectCard;
+
+import React from "react";
+
+function ProjectCard({ src, link, h3, h4, p, githubLink }) {
   return (
-    <a href={link} target="_blank">
-      <img className="hover" src={src} alt={`${h3} logo`} />
-      <h3>{h3}</h3>
-      <h4>{h4}</h4>
-      <p>{p}</p>
-    </a>
+    <div className="project-card">
+      <a href={link} target="_blank" rel="noopener noreferrer">
+        <img className="hover" src={src} alt={`${h3} logo`} />
+        <h3>{h3}</h3>
+        <h4>{h4}</h4>
+        <p>{p}</p>
+      </a>
+      {githubLink && (
+        <a href={githubLink} target="_blank" rel="noopener noreferrer">
+          <h4>Github Link</h4>
+          {/* <img
+            className="github-icon"
+            src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+            alt="GitHub link"
+          /> */}
+        </a>
+      )}
+    </div>
   );
 }
 
--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -16,31 +16,59 @@ function Projects() {
           link="https://auraforecast.netlify.app/"
           h3="Aura Forecast"
           h4="HTML, CSS, JS"
-          github="https://github.com/Pritanjan/Weather/tree/main"
+          githubLink="https://github.com/Pritanjan/Weather/tree/main"
           p="Weather App"
         />
 
+        {/*  CHECK */}
         <ProjectCard
           src={socialSphere}
           link="https://socialsphere-project.onrender.com/login"
           h3="Social Sphere"
+          githubLink=""
           h4="ReactJS , MongoDB, ExpressJS, NodeJS , PostMan , TailWind CSS"
           p="Social Media App"
         />
 
+        {/*  CHECK */}
+
         <ProjectCard
           src={TicTacToe}
-          link="https://github.com/Pritanjan/Pritanjan_Portfolio"
-          h3="Tic-Tac-Toe"
+          link="https://xoxo-grid.netlify.app/"
+          githubLink="https://github.com/Pritanjan/Tic-Tac-Toe"
+          h3="XOXO Grid"
           h4="HTML, CSS, JS"
           p="Game"
         />
 
         <ProjectCard
           src={fitLift}
-          link="https://github.com/Pritanjan/Pritanjan_Portfolio"
-          h3="FitLift"
-          p="Fitness App"
+          link="https://syncclock.netlify.app/"
+          githubLink="https://github.com/Pritanjan/Digital-Clock"
+          h3="syncclock"
+          h4="HTML, CSS, JS"
+          p="Clock App"
+          //  It shows the digital clock
+        />
+
+        <ProjectCard
+          src={fitLift}
+          link="https://arithmo.netlify.app"
+          githubLink="https://github.com/Pritanjan/Calc"
+          h3="Calculator"
+          h4="HTML, CSS, JS"
+          p="Calculator"
+          // This project is a versatile and user-friendly tool designed to meet your everyday mathematical needs.
+        />
+
+        <ProjectCard
+          src={fitLift}
+          link="https://mission-mode.netlify.app"
+          githubLink="https://github.com/Pritanjan/To-Do-List-1"
+          h3="To Do List"
+          h4="HTML, CSS, JS"
+          p="MissionMode"
+          // It is a simple and efficient task management app designed to help you stay organized and on top of your daily tasks
         />
       </div>
     </section>
